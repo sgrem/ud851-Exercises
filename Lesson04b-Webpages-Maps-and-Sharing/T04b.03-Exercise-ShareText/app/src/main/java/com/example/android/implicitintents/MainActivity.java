12180a6495bbbe1059835807415899b5335107a7
Steve Gremban <steve.gremban@gmail.com>
2017-10-27 00:27:41 -0500

    T04b.03 Exercise ShareText

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -66,10 +67,12 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickShareTextButton(View v) {
-        // TODO (5) Specify a String you'd like to share
+        // TODO COMPLETED (5) Specify a String you'd like to share
+        String textToShare = "This is the winter of our discontent...";
 
-        // TODO (6) Replace the Toast with shareText, passing in the String from step 5
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        // TODO COMPLETED (6) Replace the Toast with shareText, passing in the String from step 5
+        // Toast.makeText(this, "TODO COMPLETED : Share text when this is clicked", Toast.LENGTH_LONG).show();
+        shareText(textToShare);
     }
 
     /**
@@ -143,12 +146,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (1) Create a void method called shareText that accepts a String as a parameter
+    // TODO COMPLETED (1) Create a void method called shareText that accepts a String as a parameter
     // Do steps 2 - 4 within the shareText method
+    public void shareText(String textToShare){
+        // TODO COMPLETED (2) Create a String variable called mimeType and set it to "text/plain"
+        String mimeType = "text/plain";
 
-        // TODO (2) Create a String variable called mimeType and set it to "text/plain"
+        // TODO COMPLETED (3) Create a title for the chooser window that will pop up
+        String title = "Learning How to Share";
 
-        // TODO (3) Create a title for the chooser window that will pop up
+        // TODO COMPLETED (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+        ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setText(textToShare)
+                .setChooserTitle(title)
+                .startChooser();
 
-        // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+    }
 }
